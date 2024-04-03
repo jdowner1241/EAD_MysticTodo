a5acec037dc3d939c8a7f3b0ada28417be830225
jdowner1241 <109806567+jdowner1241@users.noreply.github.com>
2024-04-02 21:38:10 -0500

    Update 1.1 - Database added with Name(MysticToDo) - 3 identity class created on the Data folder. This includes: BaseEntity.cs, Reminder.cs and TimeFrame.cs - Reference for above would on the ApplicationDbContext.cs added.

--- WebAppRedo/Data/Migrations/20240403021950_AddedReminders.Designer.cs
+++ WebAppRedo/Data/Migrations/20240403021950_AddedReminders.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAppRedo.Data;
 
@@ -11,9 +12,11 @@ using WebAppRedo.Data;
 namespace WebAppRedo.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240403021950_AddedReminders")]
+    partial class AddedReminders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -273,7 +276,7 @@ namespace WebAppRedo.Data.Migrations
 
                     b.HasIndex("TimeFrameId");
 
-                    b.ToTable("Reminders", (string)null);
+                    b.ToTable("Reminders");
                 });
 
             modelBuilder.Entity("WebAppRedo.Data.TimeFrame", b =>
@@ -290,7 +293,7 @@ namespace WebAppRedo.Data.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("TimeFrames", (string)null);
+                    b.ToTable("TimeFrames");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
