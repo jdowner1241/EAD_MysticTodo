b8e1ae4fc8d055e6e2b3f43fd57f9714f11f0cac
jdowner1241 <109806567+jdowner1241@users.noreply.github.com>
2024-05-02 13:13:33 -0500

    Solution updated to only have the two projects

--- WebApp.MysiticToDo/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace WebApp.MysiticToDo.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- WebApp.MysiticToDo/Data/Script/03_ViewTableData.sql
+++
@@ -1,4 +0,0 @@
-SELECT TOP (1000) [Timeframe_Key]
-      ,[Timeframe_Name]
-      ,[TimeFrame_IntervalCode]
-  FROM [MysticToDo_DB].[dbo].[Timeframe]
